c50af7964507fdb900517c0e78889a75fe5815dc
Nicolas Tournier <n1c0l4s.tournier@hotmail.fr>
2014-01-12 15:31:27 +0100

    Initial commit (and also my first commit on Github)
    PHPokeAPI a basic wrapper for PokeAPI (http://pokeapi.co/)

---
+++ README.md
@@ -0,0 +1,48 @@
+PHPokeAPI
+=========
+
+A Basic PHP wrapper for PokeAPI (http://pokeapi.co/)
+
+
+Requirements
+============
+
+* PHP 5.2+
+* [cURL](http://php.net/curl) PHP Extension
+* [JSON](http://php.net/json) PHP Extension
+
+
+Installation
+============
+
+Simply include the `/lib/pokeAPI.php` file into your script.
+
+	<?php
+
+	require './lib/pokeAPI.php';
+	$api = new PokeAPI();
+
+	?>
+	
+Methods
+=======
+
+`$this->getPokedex();` Returns the Pokedex.
+
+`$this->getPokemon($id);` Returns the Pokemon, where `$id` corresponds to the number in the national pokedex.
+
+`$this->getType($id);` Returns type information
+
+`$this->getMove($id);` 
+
+`$this->getAbility($id);` 
+
+`$this->getEggGroup($id);` 
+
+`$this->getDescription($id);` 
+
+`$this->getSprite($id);` Returns the URL of the Pokemon sprite
+
+`$this->getGame($id);` 
+
+`$this->getRessource($id);` Return the information corresponding to `ressource_uri` contained in an previous request.
---
+++ lib/pokeAPI.php
@@ -0,0 +1,204 @@
+<?php
+
+/******************************************************
+ * PokeAPI - PH[P]okeAPI                              *
+ * ================================================== *
+ * @author       Nicolas Tournier <@n1c0l4stournier>  *
+ * @version      1.0                                  *
+ *****************************************************/
+
+class PokeAPI {
+
+	public $endpoint = "http://pokeapi.co/api/v1";
+
+	/**
+	 *	testing if cURL is installed
+	 *
+	 */
+	public function __construct() {
+
+        if(!function_exists('curl_init'))
+            throw new Exception('The cURL module could not be loaded, and is needed to retrieve API data.');
+
+    }
+
+	/**
+	 *	
+	 *
+	 */
+	public function getPokedex() {
+
+		$option = "pokedex";
+		return $this->get(1, $option);
+
+	}
+
+
+	/**
+	 *	
+	 *
+	 */
+	public function getPokemon($id = NULL) {
+
+		$option = "pokemon";
+		return $this->get($id, $option);
+
+	}
+
+
+	/**
+	 *	
+	 *
+	 */
+	public function getType($id = NULL) {
+
+		$option = "type";
+		return $this->get($id, $option);
+
+	}
+
+
+	/**
+	 *	
+	 *
+	 */
+	public function getMove($id = NULL) {
+
+		$option = "move";
+		return $this->get($id, $option);
+
+	}
+
+
+	/**
+	 *	
+	 *
+	 */
+	public function getAbility($id = NULL) {
+
+		$option = "ability";
+		return $this->get($id, $option);
+
+	}
+
+
+	/**
+	 *	
+	 *
+	 */
+	public function getEggGroup($id = NULL) {
+
+		$option = "egg";
+		return $this->get($id, $option);
+
+	}
+
+
+	/**
+	 *	
+	 *
+	 */
+	public function getDescription($id = NULL) {
+
+		$option = "description";
+		return $this->get($id, $option);
+
+	}
+
+
+	/**
+	 *	
+	 *
+	 */
+	public function getSprite($id = NULL) {
+
+		$option = "sprite";
+		return $this->get($id, $option);
+
+	}
+
+
+	/**
+	 *	
+	 *
+	 */
+	public function getGame($id = NULL) {
+
+		$option = "game";
+		return $this->get($id, $option);
+
+	}
+
+
+	/**
+	 *	ressource_uri = /api/vi/{string}/{int}
+	 *	string $option = {string}
+	 *	int $id = {int}
+	 */
+	public function getRessource($uri)
+	{
+		$uri = explode("/", $uri);
+		
+		$id = NULL; 
+		$option = NULL;
+		foreach ($uri as $d) {
+			if ($d != 'api' && $d != 'v1' && !empty($d)) {
+				if(is_numeric($d))
+					$id = $d;
+				else
+					$option = $d;
+			}
+		}
+		return $this->get($id, $option);
+	}
+
+
+	/**
+	 *	return the decoded JSON data into stdClass Object
+	 *
+	 */
+	private function get($id = NULL, $option = NULL) {
+
+		if ($id == NULL || $option == NULL)
+			return FALSE;
+		else 
+			return $this->curl($this->endpoint . "/" . $option . "/" . $id . "/");
+
+	}
+
+
+	/**
+	 *	send request to the API and return the decoded JSON data into stdClass Object
+	 *
+	 */
+	private function curl($url = NULL) {
+
+		if ($url == NULL)
+			return FALSE;
+
+		else {
+
+	        $ch = curl_init();
+	        curl_setopt($ch, CURLOPT_URL, $url);
+	        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+	        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+	        curl_setopt($ch, CURLOPT_AUTOREFERER, true);
+
+	        // sending the request to the API
+	        $result = curl_exec($ch);
+
+	        // decoding the JSON data
+	        $result = json_decode($result);
+
+	        if (!$result)
+	        	return FALSE;
+
+	        return $result;
+		}
+	}
+
+}
+
+?>
