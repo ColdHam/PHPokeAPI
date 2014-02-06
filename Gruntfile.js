720219b4769cf699fe86556062970f4c5a9d380c
Nicolas Tournier <nicolas.tournier@outlook.com>
2014-02-06 17:54:49 +0100

    PHPCS
    improve descriptions

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,7 +11,7 @@ module.exports = function(grunt)
 			},
 			options: {
 				bin: "phpcs",
-				standard: "PSR1"
+				standard: "PHPCS"
 			}
 		},
 
--- phpokeapi/PokeApi.php
+++ phpokeapi/PokeApi.php
@@ -1,146 +1,258 @@
 <?php
 
+/**
+ * Simple PHP wrappar for PokeAPI : http://pokeapi.co/
+ *
+ * PHP version 5
+ *
+ * @category Wrapper
+ * @package  PokeApi
+ * @author   Nicolas Tournier <nicolas.tournier@outlook.com>
+ * @license  GNU General Public License version 3 (GPLv3)
+ * @link     https://github.com/n1c0l4stournier/PHPokeAPI
+ *
+ */
+
 namespace PokeApi\PokeApi;
 
+/**
+ * PokeAPI class
+ *
+ * @category Wrapper
+ * @package  PokeApi
+ * @author   Nicolas Tournier <nicolas.tournier@outlook.com>
+ * @license  GNU General Public License version 3 (GPLv3)
+ * @link     https://github.com/n1c0l4stournier/PHPokeAPI
+ *
+ */
 class PokeApi
 {
-    
-    public $endpoint = "http://pokeapi.co/api/v1";
 
+    public $endpoint = 'http://pokeapi.co/api/v1';
+
+
+    /**
+     * __construct
+     *
+     * @throws Exception
+     *
+     */
     public function __construct()
     {
-
-        if (!function_exists('curl_init')) {
-
+        if (function_exists('curl_init') === false) {
             throw new Exception('The cURL module could not be loaded, and is needed to retrieve API data.');
-            
         }
 
-    }
+    }//end __construct()
 
+
+    /**
+     * getPokedex
+     *
+     * @return Object
+     *
+     */
     public function getPokedex()
     {
+        $option = 'pokedex';
+        return $this->_get(1, $option);
 
-        $option = "pokedex";
-        return $this->get(1, $option);
+    }//end getPokedex()
 
-    }
 
-    public function getPokemon($id = null)
+    /**
+     * getPokemon
+     *
+     * @param int $id Pokemon id in the national pokedex
+     *
+     * @return Object
+     *
+     */
+    public function getPokemon($id=null)
     {
+        $option = 'pokemon';
+        return $this->_get($id, $option);
 
-        $option = "pokemon";
-        return $this->get($id, $option);
+    }//end getPokemon()
 
-    }
 
-    public function getType($id = null)
+    /**
+     * getType
+     *
+     * @param int $id type id according to the API
+     *
+     * @return Object
+     *
+     */
+    public function getType($id=null)
     {
+        $option = 'type';
+        return $this->_get($id, $option);
 
-        $option = "type";
-        return $this->get($id, $option);
+    }//end getType()
 
-    }
 
-    public function getMove($id = null)
+    /**
+     * getMove
+     *
+     * @param int $id move id according to the API
+     *
+     * @return Object
+     *
+     */
+    public function getMove($id=null)
     {
+        $option = 'move';
+        return $this->_get($id, $option);
 
-        $option = "move";
-        return $this->get($id, $option);
+    }//end getMove()
 
-    }
 
-    public function getAbility($id = null)
+    /**
+     * getAbility
+     *
+     * @param int $id Type ability id according to the API
+     *
+     * @return Object
+     *
+     */
+    public function getAbility($id=null)
     {
+        $option = 'ability';
+        return $this->_get($id, $option);
 
-        $option = "ability";
-        return $this->get($id, $option);
+    }//end getAbility()
 
-    }
 
-    public function getEggGroup($id = null)
+    /**
+     * getEggGroup
+     *
+     * @param int $id egg group id according to the API
+     *
+     * @return Object
+     *
+     */
+    public function getEggGroup($id=null)
     {
+        $option = 'egg';
+        return $this->_get($id, $option);
 
-        $option = "egg";
-        return $this->get($id, $option);
+    }//end getEggGroup()
 
-    }
 
-    public function getDescription($id = null)
+    /**
+     * getDescription
+     *
+     * @param int $id deescription id of a Pokemon according to the API
+     *
+     * @return Object
+     *
+     */
+    public function getDescription($id=null)
     {
+        $option = 'description';
+        return $this->_get($id, $option);
 
-        $option = "description";
-        return $this->get($id, $option);
+    }//end getDescription()
 
-    }
 
-    public function getSprite($id = null)
+    /**
+     * getSprite
+     *
+     * @param int $id sprite id according to the API
+     *
+     * @return Object
+     *
+     */
+    public function getSprite($id=null)
     {
+        $option = 'sprite';
+        return $this->_get($id, $option);
 
-        $option = "sprite";
-        return $this->get($id, $option);
+    }//end getSprite()
 
-    }
 
-    public function getGame($id = null)
+    /**
+     * getGame
+     *
+     * @param int $id game id according to the API
+     *
+     * @return Object
+     *
+     */
+    public function getGame($id=null)
     {
+        $option = 'game';
+        return $this->_get($id, $option);
 
-        $option = "game";
-        return $this->get($id, $option);
+    }//end getGame()
 
-    }
 
+    /**
+     * getRessource
+     *
+     * @param string $uri uri_ressource
+     *
+     * @return Object
+     *
+     */
     public function getRessource($uri)
     {
-        
-        $id = null;
+        $id     = null;
         $option = null;
-        $uri = explode("/", $uri);
+        $uri    = explode('/', $uri);
 
         foreach ($uri as $d) {
-
-            if ($d != 'api' && $d != 'v1' && !empty($d)) {
-
-                if (is_numeric($d)) {
-
+            if ($d !== 'api' && $d !== 'v1' && empty($d) === false) {
+                if (is_numeric($d) === true) {
                     $id = $d;
-
                 } else {
-
                     $option = $d;
-
                 }
             }
-
         }
 
-        return $this->get($id, $option);
-    }
+        return $this->_get($id, $option);
 
-    private function get($id = null, $option = null)
+    }//end getRessource()
+
+
+    /**
+     * _get
+     *
+     * @param int    $id     Id
+     * @param string $option Option
+     *
+     * @throws Exception
+     *
+     * @return Object
+     *
+     */
+    private function _get($id=null, $option=null)
     {
-
-        if ($id == null || $option == null) {
-
+        if ($id === null || $option === null) {
             throw new Exception('Empty data.');
-
         } else {
-
-            return $this->curl($this->endpoint . "/" . $option . "/" . $id . "/");
-
+            return $this->_curl($this->endpoint.'/'.$option.'/'.$id.'/');
         }
 
-    }
+    }//end _get()
 
-    private function curl($url = null)
+
+    /**
+     * _curl
+     *
+     * @param string $url PokeAPI url.
+     *
+     * @return Object
+     *
+     */
+
+    private function _curl($url=null)
     {
-
-        if ($url == null) {
-
+        if ($url === null) {
             return false;
-
         } else {
-
             $ch = curl_init();
             curl_setopt($ch, CURLOPT_URL, $url);
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -149,19 +261,18 @@ class PokeApi
             curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
             curl_setopt($ch, CURLOPT_AUTOREFERER, true);
 
-            // sending the request to the API
+            // Sending the request to the API.
             $result = curl_exec($ch);
 
-            // decoding the JSON data
+            // Decoding the JSON data.
             $result = json_decode($result);
 
-            if (!$result) {
-
-                return false;
-
-            }
-
             return $result;
-        }
-    }
-}
+        }//end if
+
+    }//end _curl()
+
+
+}//end class
+
+?>
